2fd2ca090060c3a490d2283ffc6c97cbf950fcd9
Victor Jimenez <vojimene@ncsu.edu>
2025-01-07 14:10:00 -0500

    added 'pages' to the frontend

--- toDoAppFrontend/website.js
+++ toDoAppFrontend/website.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const listsPage = document.getElementById('lists-page');
+    const itemsPage = document.getElementById("items-page")
+    const clickForItemsPage = document.querySelector(".lists")
+    console.log(clickForItemsPage)
+    const backButton = document.getElementById('back-to-lists');
     const hamburger = document.getElementById("hamburger");
     const sidebar = document.getElementById("sidebar");
     const mainContent = document.querySelector(".container");
     console.log("not clicked");
+
     function displaySidebar () {
         console.log("clicked");
         if (sidebar.style.left === "-250px" || sidebar.style.left === ""){
@@ -18,7 +24,20 @@ document.addEventListener("DOMContentLoaded", function() {
         }
         
     }
+    function displayListsPage () {
+        itemsPage.style.display = `none`
+        listsPage.style.display = `block`
+    }
+    function displayItemsPage () {
+        console.log("was i clicked to show items page")
+        listsPage.style.display = `none`
+        console.log(listsPage.style.display)
+        itemsPage.style.display = `block`
+    }
     
     hamburger.addEventListener("click", displaySidebar)    
+    
+    backButton.addEventListener("click", displayListsPage)
+    clickForItemsPage.addEventListener("click", displayItemsPage)
 });
 
