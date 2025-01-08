d49ab5b99ab7072fdcdf5f193e3d03dd5adf2c70
Victor Jimenez <vojimene@ncsu.edu>
2025-01-08 12:41:28 -0500

    made transtions work for both 'pages'

--- toDoAppFrontend/website.js
+++ toDoAppFrontend/website.js
@@ -1,4 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
+    function loadSideBar (){
+        const sidebarHTML = `
+        <div class="hamburger-menu" id="hamburger">
+            &#9776; <!-- This is the hamburger icon (three horizontal lines) -->
+        </div>
+        <nav class="sidebar" id="sidebar">
+            <ul class="sidebar-list">
+                <li class="sidebar-item">
+                    <div class="circle plus"></div>
+                    <p> Create List </p>
+                </li>
+                <li class="sidebar-item">
+                    <div class="container-fa"><i class="fa fa-search"></i></div>
+                    <p> Search </p>    
+                </li>
+                <li class="sidebar-item" id = "back-to-lists">
+                    <div class="gg-arrow-left-o"></div>
+                    <p> Back to Lists</p>
+            </ul>
+        </nav>`
+        document.body.insertAdjacentHTML('afterbegin', sidebarHTML);
+        console.log("should be succesful")
+    }
+    loadSideBar();
     const listsPage = document.getElementById('lists-page');
     const itemsPage = document.getElementById("items-page")
     const clickForItemsPage = document.querySelector(".lists")
@@ -7,32 +31,35 @@ document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById("hamburger");
     const sidebar = document.getElementById("sidebar");
     const mainContent = document.querySelector(".container");
+    
     console.log("not clicked");
-
+    
     function displaySidebar () {
         console.log("clicked");
         if (sidebar.style.left === "-250px" || sidebar.style.left === ""){
             console.log(":)")
             sidebar.style.left = `0`;
 
-            mainContent.style.marginLeft = `140px`;            
+               
+            listsPage.style.marginLeft = `140px`         
+            itemsPage.style.marginLeft = `140px`
         }
         else {
             console.log(sidebar.style.left)
             sidebar.style.left = `-250px`;
-            mainContent.style.marginLeft = `0px`;
+            
+            listsPage.style.marginLeft = `0px`
+            itemsPage.style.marginLeft = `0px`
         }
         
     }
     function displayListsPage () {
-        itemsPage.style.display = `none`
-        listsPage.style.display = `block`
+        itemsPage.style.visibility = `hidden`
+        listsPage.style.visibility = `visible`
     }
     function displayItemsPage () {
-        console.log("was i clicked to show items page")
-        listsPage.style.display = `none`
-        console.log(listsPage.style.display)
-        itemsPage.style.display = `block`
+        listsPage.style.visibility = `hidden`
+        itemsPage.style.visibility = `visible`
     }
     
     hamburger.addEventListener("click", displaySidebar)    
