b28f55107a7dcee228b2075405b77835ae69c2c9
Victor Jimenez <vojimene@ncsu.edu>
2025-01-10 16:12:38 -0500

    finished adding dummy items for items page and developed better ideas for how to make the website

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ first: poetry install
 How to run tests: poetry run test
 How to run server: poetry run run-server
 to add libraries/dependencies you would have to do: poetry add [name of libary]
+
+Frontend things I need to do:
+1/8/2025
+Think of how I want to display items.
+Ill have to think about the fact that ill have to make and recieve the calls to the database so first I 
+should make dummy items
+1/10/2025
+Created the dummy items now all thats left is to make sure that the dummy items show their description
+and whether are done or not. Thinking maybe ill show the descriptions after clicking on the item.
+At least for mobile. maybe for on computer I show it to the side of the name of the task item to do
+so that way I can use that extra space. In fact I could probably do something similar with the list display as I can show how many items out of total items are complete. Same thing can be done for mobile next to item name to show if its complete or not instead of showing it once clicking on the item.
+
+I should probably add some more comments to my backend and frontend
--- toDoAppFrontend/website.js
+++ toDoAppFrontend/website.js
@@ -20,29 +20,24 @@ document.addEventListener("DOMContentLoaded", function() {
             </ul>
         </nav>`
         document.body.insertAdjacentHTML('afterbegin', sidebarHTML);
-        console.log("should be succesful")
     }
     loadSideBar();
     const listsPage = document.getElementById('lists-page');
     const itemsPage = document.getElementById("items-page")
-    const clickForItemsPage = document.querySelector(".lists")
-    console.log(clickForItemsPage)
+    const listsContainer = document.querySelector(".lists")
+    
     const backButton = document.getElementById('back-to-lists');
     const hamburger = document.getElementById("hamburger");
     const sidebar = document.getElementById("sidebar");
-    const mainContent = document.querySelector(".container");
     
-    console.log("not clicked");
     
     function displaySidebar () {
-        console.log("clicked");
         if (sidebar.style.left === "-250px" || sidebar.style.left === ""){
-            console.log(":)")
             sidebar.style.left = `0`;
 
                
-            listsPage.style.marginLeft = `140px`         
-            itemsPage.style.marginLeft = `140px`
+            listsPage.style.marginLeft = `130px`         
+            itemsPage.style.marginLeft = `130px`
         }
         else {
             console.log(sidebar.style.left)
@@ -57,14 +52,43 @@ document.addEventListener("DOMContentLoaded", function() {
         itemsPage.style.visibility = `hidden`
         listsPage.style.visibility = `visible`
     }
-    function displayItemsPage () {
-        listsPage.style.visibility = `hidden`
-        itemsPage.style.visibility = `visible`
+    function displayItemsPage (listId, listName) {
+
+        console.log(`Loading items for List ID: ${listId}, List Name: ${listName}`);
+        listsPage.style.visibility = `hidden`;
+        itemsPage.style.visibility = `visible`;
+
+        // Example: Update items page with the list's name or content
+    
+       
+        const itemHTML = `
+        <h3>${listId}</h3>
+        <div class="container">
+            <h2 class="title-items">${listName}</h2>
+            
+            <ul class="items">
+                <li class="item" data-id="1" data-name="Groceries">Groceries but not same</li>
+                <li class="item" data-id="2" data-name="Work Tasks">Work Tasks</li>
+                <li class="item" data-id="3" data-name="Personal Goals">Personal Goals</li>
+            </ul>
+        </div>`
+        
+        itemsPage.insertAdjacentHTML('afterbegin', itemHTML);
+        
     }
     
     hamburger.addEventListener("click", displaySidebar)    
     
     backButton.addEventListener("click", displayListsPage)
-    clickForItemsPage.addEventListener("click", displayItemsPage)
+    listsContainer.addEventListener('click', function (event) {
+        console.log("was the container clicked")
+        const listItem = event.target.closest(".list")
+        console.log(listItem)
+        if (listItem) {
+            const listId = listItem.dataset.id; // Access data-id
+            const listName = listItem.dataset.name; // Access data-name
+            displayItemsPage(listId, listName);
+        }
+    });
 });
 
